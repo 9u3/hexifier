5dc9ae895e972a09042f5805b384eeec4e844f24
Nox <48463140+9u3@users.noreply.github.com>
2021-04-08 18:10:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from distutils.core import setup
+setup(
+  name = 'hexifier',
+  packages = ['hexifier'],
+  version = '1',
+  license='MIT',
+  description = 'hex2rgb / rgb2hex',
+  author = 'Nox Protogen',
+  author_email = 'cfym.gamer@gmail.com',
+  url = 'https://github.com/9u3/hexifier',
+  download_url = 'https://github.com/9u3/hexifier/tree/master/hexifier',
+  keywords = ['wrapper', 'api', 'random', 'hex', 'rgb'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
